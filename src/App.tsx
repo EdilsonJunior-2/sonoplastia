12491e54b161f2406bf57364632063094c2915cd
Edilson <juniorse13@gmail.com>
2022-06-17 22:11:59 -0300

    Site V1

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import "./assets/styles/main.scss";
+import { data } from "./data";
+
+function App() {
+  const baseAudioURL = "src/assets/audios";
+  return (
+    <div className="App">
+      {data.map((d) => (
+        <div key={d.page}>
+          <div className="font-size-h2">{d.name}</div>
+          {d.buttons.map((name) => (
+            <button
+              className="button-primary small-rounded p-horizontal-2 p-vertical-1 m-all-1 font-size-thin font-weight-bold"
+              key={name}
+              onClick={() => {
+                const audio = new Audio(
+                  `${baseAudioURL}/${d.page}/${name}.m4a`
+                );
+                audio.play();
+              }}
+            >
+              {name}
+            </button>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
